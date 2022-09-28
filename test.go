72af873fa0cdeaae2fb68be38bd2e5f0405d370d
ZaripovAlexey <zaripovalex@mail.ru>
2022-09-28 17:32:14 +0500

    ПРошел первый урок

--- test.go
+++ test.go
@@ -7,4 +7,8 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println("1+1=", 1+1)
+	fmt.Println("1+1=", 1.0+1.0)
+	fmt.Println("len", len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello " + "World")
 }
